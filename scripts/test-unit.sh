e0650cdb7b59ef9da25f7c0bbaa3730862bf32ee
Chris Tarazi <ctarazi@pivotal.io>
2019-08-07 14:34:17 -0700

    Undo hiding output in test script
    Co-authored-by: Joshua Aresty <joshua.aresty@emc.com>

--- scripts/test-unit.sh
+++ scripts/test-unit.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-set -eu
+set -eux
 set -o pipefail
 
 source_code=$(dirname $0)/..
 source ${source_code}/.envrc
-{
-  go mod tidy && go mod verify
-} &> /dev/null
 pushd ${source_code}
   ginkgo -v --race --randomizeAllSpecs
 popd
